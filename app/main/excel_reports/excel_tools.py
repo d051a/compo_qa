202c9e833fd610ed427eca426e87a04bb0cd8a73
d051a <akargavin@gmail.com>
2020-10-07 23:08:15 +0300

    add new excel report

--- app/main/excel_reports/excel_tools.py
+++ app/main/excel_reports/excel_tools.py
@@ -22,46 +22,55 @@ def expands_columns_width(worksheet):
     return worksheet
 
 
-def create_excel_cheet(workbook, reports_list, model_included_columns, vertical=False):
-    row_num = 1
-    colon_num = 1
-    worksheet = workbook.create_sheet(reports_list.model._meta.verbose_name_plural.title(), 0)
+def get_columns_ids(model_included_columns, reports_list):
     columns = []
     columns_ids = []
     db_model_fields = [field.name for field in reports_list.model._meta.fields]
-    thin_border = Border(left=Side(style='thin'), right=Side(style='thin'), top=Side(style='thin'),
-                         bottom=Side(style='thin'))
 
     for num, field in enumerate(model_included_columns):
         if field in db_model_fields:
             columns.append(reports_list.model._meta.get_field(field).verbose_name)
             columns_ids.append(db_model_fields.index(field))
+    return columns, columns_ids
+
+
+def generate_table_data(worksheet, reports_list, model_included_columns, vertical=False, start_row_num=1):
+    columns_and_ids = get_columns_ids(model_included_columns, reports_list)
+    columns = columns_and_ids[0]
+    columns_ids = columns_and_ids[1]
+    colon_or_row_num = 1
+    thin_border = Border(left=Side(style='thin'), right=Side(style='thin'), top=Side(style='thin'),
+                         bottom=Side(style='thin'))
 
     # создание шапки таблицы
-    for elem_num, column_title in enumerate(columns, 1):
+    for elem_num, column_title in enumerate(columns, start_row_num):
         if vertical:
-            cell = worksheet.cell(row=elem_num, column=colon_num)
+            cell = worksheet.cell(row=elem_num, column=colon_or_row_num)
         else:
-            cell = worksheet.cell(row=row_num, column=elem_num)
+            cell = worksheet.cell(row=colon_or_row_num, column=elem_num)
         cell.value = column_title
         cell.border = thin_border
         cell.font = Font(bold=True)
 
     # заполнение таблицы даными
     for stat in reports_list.values_list():
-        row_num += 1
-        colon_num += 1
+        colon_or_row_num += 1
         row = [stat[stat_id] for stat_id in columns_ids]
-        for elem_num, cell_value in enumerate(row, 1):
+        for elem_num, cell_value in enumerate(row, start_row_num):
             if vertical:
-                cell = worksheet.cell(row=elem_num, column=colon_num)
+                cell = worksheet.cell(row=elem_num, column=colon_or_row_num)
             else:
-                cell = worksheet.cell(row=row_num, column=elem_num)
+                cell = worksheet.cell(row=colon_or_row_num, column=elem_num)
             cell.value = cell_value
             cell.border = thin_border
+    return worksheet
 
+
+def create_excel_cheet(workbook, reports_list, model_included_columns, vertical=False):
+    worksheet = workbook.create_sheet(reports_list.model._meta.verbose_name_plural.title(), 0)
+    worksheet_with_data = generate_table_data(worksheet, reports_list, model_included_columns, vertical=False)
     # увеличение ширины колонок
-    expands_columns_width(worksheet)
+    expands_columns_width(worksheet_with_data)
     return workbook
 
 
@@ -111,6 +120,63 @@ def create_excel_cheet_for_stats(workbook, reports_list, report_stats_model, pri
 
         # увеличение ширины колонок
         expands_columns_width(worksheet)
+    return workbook
+
+
+def create_excel_net_draw_cheet(workbook,
+                                net_compile_reports,
+                                net_compile_included_columns,
+                                draw_imgs_reports,
+                                draw_imgs_included_columns,
+                                vertical=False):
+    worksheet = workbook.create_sheet('Сводный отчет', 0)
+    colon_or_row_num = 1
+    columns = []
+    columns_ids = []
+    db_model_fields = [field.name for field in net_compile_reports.model._meta.fields]
+    thin_border = Border(left=Side(style='thin'), right=Side(style='thin'), top=Side(style='thin'),
+                         bottom=Side(style='thin'))
+
+    for num, field in enumerate(net_compile_included_columns):
+        if field in db_model_fields:
+            columns.append(net_compile_reports.model._meta.get_field(field).verbose_name)
+            columns_ids.append(db_model_fields.index(field))
+
+    # создание шапки таблицы
+    for elem_num, column_title in enumerate(columns, 1):
+        if vertical:
+            cell = worksheet.cell(row=elem_num, column=colon_or_row_num)
+        else:
+            cell = worksheet.cell(row=colon_or_row_num, column=elem_num)
+        cell.value = column_title
+        cell.border = thin_border
+        cell.font = Font(bold=True)
+
+    colon_or_row_num += 1
+    # заполнение таблицы даными о сборках сети
+    for stat in net_compile_reports.values_list():
+        row = [stat[stat_id] for stat_id in columns_ids]
+        for elem_num, cell_value in enumerate(row, 1):
+            if vertical:
+                worksheet.merge_cells(start_row=elem_num, start_column=colon_or_row_num, end_row=elem_num,
+                                      end_column=colon_or_row_num+1)
+                cell = worksheet.cell(row=elem_num, column=colon_or_row_num)
+                cell.value = cell_value
+                cell.border = thin_border
+                cell = worksheet.cell(row=elem_num, column=colon_or_row_num+1)
+                cell.border = thin_border
+            else:
+                cell = worksheet.cell(row=colon_or_row_num, column=elem_num)
+                cell.value = cell_value
+                cell.border = thin_border
+                cell = worksheet.cell(row=colon_or_row_num+1, column=elem_num)
+                cell.border = thin_border
+
+            cell.border = thin_border
+        colon_or_row_num += 2
+    start_row_num = len(net_compile_included_columns) + 1
+    generate_table_data(worksheet, draw_imgs_reports, draw_imgs_included_columns, vertical=True, start_row_num=start_row_num)
+    # expands_columns_width(worksheet)
     return workbook
 
 
--- app/main/excel_reports/views_excel.py
+++ app/main/excel_reports/views_excel.py
@@ -1,12 +1,18 @@
 from main.models import Statistic,  MetricReport, Chaos, DrawImgsReport,\
     NetCompileReport, DrawImgsStat, NetCompilationStat
-from main.excel_reports.excel_tools import create_excel_cheet, create_excel_cheet_for_stats
+from main.excel_reports.excel_tools import create_excel_cheet, create_excel_cheet_for_stats, create_excel_net_draw_cheet
 from django.http import HttpResponse
 from datetime import datetime
 from openpyxl import Workbook
 
 
 def metric_report_export_to_xlsx(request, metric_report_id):
+    """
+    Генерация excel-отчета со страницы снятия общих метрик /metrics/<report_ID>/
+    :param request:
+    :param metric_report_id:
+    :return:
+    """
     metrics_report = MetricReport.objects.get(pk=metric_report_id)
     net_compile_reports = NetCompileReport.objects.filter(metric_report=metrics_report)
     draw_imgs_reports = DrawImgsReport.objects.filter(metric_report=metrics_report)
@@ -56,11 +62,18 @@ def metric_report_export_to_xlsx(request, metric_report_id):
     workbook = create_excel_cheet(workbook, net_compile_reports, net_compile_reports_draw_fields, vertical=True)
     workbook = create_excel_cheet(workbook, draw_imgs_reports, draw_imgs_reports_draw_fields, vertical=True)
     workbook = create_excel_cheet(workbook, metrics_report_common_statistic, metrics_report_common_statistic_draw_fields)
+    workbook = create_excel_net_draw_cheet(workbook, net_compile_reports, net_compile_reports_draw_fields, draw_imgs_reports, draw_imgs_reports_draw_fields, vertical=True)
     workbook.save(response)
     return response
 
 
 def chaos_stats_export_to_xlsx(request, chaos_id):
+    """
+    Генерация excel-отчета со страницы (chaoses/<chaos_ID>/stats) по всем сборкам\отрисовкам
+    :param request:
+    :param chaos_id:
+    :return:
+    """
     chaos = Chaos.objects.get(pk=chaos_id)
     net_compile_reports = NetCompileReport.objects.filter(chaos=chaos).order_by('-create_date_time')
     draw_imgs_reports = DrawImgsReport.objects.filter(chaos=chaos).order_by('-create_date_time')
