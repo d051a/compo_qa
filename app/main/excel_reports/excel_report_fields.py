4c74d920bec8e2d3f2af1d27e74a81faf1538c46
d051a <akargavin@gmail.com>
2020-10-27 15:51:26 +0300

    change all metrics excel report logic

---
+++ app/main/excel_reports/excel_report_fields.py
@@ -0,0 +1,71 @@
+net_compile_short_report_draw_fields = ['create_date_time', 'status', 'date_time_finish', 'name', 'ip',
+                                        'p50', 'p75', 'p90', 'p95', 'p96', 'p97', 'p98', 'p99', 'p100']
+
+draw_imgs_stats_short_report_draw_fields = ['create_date_time', 'status', 'date_time_finish', 'name', 'ip',
+                                            'p50', 'p75', 'p90', 'p95', 'p96', 'p97', 'p98', 'p99', 'p100']
+
+net_compile_reports_draw_fields = ['create_date_time', 'status',
+                                   'p50', 'p75', 'p90', 'p95', 'p96', 'p97', 'p98', 'p99', 'p100',
+                                   'fact_total_esl', 'date_time_finish',
+                                   ]
+draw_imgs_reports_draw_fields = ['create_date_time', 'status',
+                                 'p50', 'p75', 'p90', 'p95', 'p96', 'p97', 'p98', 'p99', 'p100', 'fact_total_esl',
+                                 'drawed_esl', 'not_drawed_esl', 'date_time_finish'
+                                 ]
+draw_imgs_stat_fields = ['percent_step', 'elapsed_time', 'images_succeeded']
+net_compile_stat_fields = ['compilation_percent', 'online_esl', 'elapsed_time']
+metrics_report_common_statistic_draw_fields = ['date_time',
+                                               'total_nodes',
+                                               'inaccessible_nodes',
+                                               'total_number_routes',
+                                               'maximum_road_length',
+                                               'average_route_length',
+                                               'accessible_nodes_percent',
+                                               'elapsed_time',
+                                               'total_esl',
+                                               'online_esl',
+                                               'images_in_transit',
+                                               'images_in_draw',
+                                               'images_in_resend_queue',
+                                               'images_succeeded',
+                                               'images_failed',
+                                               'currently_scanning',
+                                               'network_mode',
+                                               'connects',
+                                               'network_mode_percent',
+                                               'voltage_average',
+                                               'voltage_current',
+                                               'voltage_max',
+                                               'bat_reserved1',
+                                               'bat_reserved2',
+                                               'bat_reserved3',
+                                               'bat_reserved4',
+                                               ]
+
+net_compile_fields_common_report = ['create_date_time', 'elapsed_time', 'final_percent', 't60', 'fact_total_esl',
+                                    'status',
+                                    ]
+draw_imgs_fields_common_report = ['p50', 'p75', 'p90', 'p95', 'p96', 'p97', 'p98', 'p99', 'p995', 'p999', 'p100',
+                                  'not_drawed_esl',
+                                  ]
+net_compile_fields_common_extended_report = ['create_date_time', 'elapsed_time', 'final_percent', 'status',
+                                             'p10', 'p20', 'p30', 'p40', 'p50', 'p60', 'p75', 'p90', 'p95', 'p96',
+                                             'p97', 'p98', 'p99', 'p995', 'p999', 'p100',
+                                             't10', 't20', 't30', 't40', 't50', 't60', 't70', 't80', 't90', 't100',
+                                             't110', 't120', 't130', 't140', 't150',
+                                             'fact_total_esl',
+                                             'success_percent',
+                                             ]
+draw_imgs_fields_common_extended_report = ['create_date_time',
+                                           'date_time_finish',
+                                           'draw_imgs_limit_mins',
+                                           'p10', 'p20', 'p30', 'p40', 'p50', 'p60', 'p75', 'p90', 'p95', 'p96',
+                                           'p97', 'p98', 'p99', 'p995', 'p999', 'p100',
+                                           't10', 't20', 't30', 't40', 't50', 't60', 't70', 't80', 't90', 't100',
+                                           't110', 't120', 't130', 't140', 't150',
+                                           'drawed_esl',
+                                           'not_drawed_esl',
+                                           'final_percent',
+                                           'draw_imgs_type',
+                                           'status',
+                                           ]
--- app/main/excel_reports/views_excel.py
+++ app/main/excel_reports/views_excel.py
@@ -4,6 +4,11 @@ from main.excel_reports.excel_tools import create_excel_cheet, create_excel_chee
 from django.http import HttpResponse
 from datetime import datetime
 from openpyxl import Workbook
+from main.excel_reports.excel_report_fields import net_compile_short_report_draw_fields,\
+    draw_imgs_stats_short_report_draw_fields, net_compile_reports_draw_fields, draw_imgs_reports_draw_fields,\
+    draw_imgs_stat_fields, net_compile_stat_fields, metrics_report_common_statistic_draw_fields,\
+    net_compile_fields_common_report, draw_imgs_fields_common_report, net_compile_fields_common_extended_report,\
+    draw_imgs_fields_common_extended_report
 
 
 def metric_report_export_to_xlsx(request, metric_report_id):
@@ -17,73 +22,6 @@ def metric_report_export_to_xlsx(request, metric_report_id):
     net_compile_reports = NetCompileReport.objects.filter(metric_report=metrics_report)
     draw_imgs_reports = DrawImgsReport.objects.filter(metric_report=metrics_report)
     metrics_report_common_statistic = Statistic.objects.filter(metric_report=metrics_report).order_by('date_time')
-
-    net_compile_reports_draw_fields = ['create_date_time', 'status',
-                                       'p50', 'p75', 'p90', 'p95', 'p96', 'p97', 'p98', 'p99', 'p100',
-                                       'fact_total_esl', 'date_time_finish',
-                                       ]
-    draw_imgs_reports_draw_fields = ['create_date_time', 'status',
-                                     'p50', 'p75', 'p90', 'p95', 'p96', 'p97', 'p98', 'p99', 'p100', 'fact_total_esl',
-                                     'drawed_esl', 'not_drawed_esl', 'date_time_finish'
-                                     ]
-    draw_imgs_stat_fields = ['percent_step', 'elapsed_time', 'images_succeeded']
-    net_compile_stat_fields = ['compilation_percent', 'online_esl', 'elapsed_time']
-    metrics_report_common_statistic_draw_fields = [
-        'date_time',
-        'total_nodes',
-        'inaccessible_nodes',
-        'total_number_routes',
-        'maximum_road_length',
-        'average_route_length',
-        'accessible_nodes_percent',
-        'elapsed_time',
-        'total_esl',
-        'online_esl',
-        'images_in_transit',
-        'images_in_draw',
-        'images_in_resend_queue',
-        'images_succeeded',
-        'images_failed',
-        'currently_scanning',
-        'network_mode',
-        'connects',
-        'network_mode_percent',
-        'voltage_average',
-        'voltage_current',
-        'voltage_max',
-        'bat_reserved1',
-        'bat_reserved2',
-        'bat_reserved3',
-        'bat_reserved4',
-    ]
-
-    net_compile_fields_common_report = ['create_date_time', 'elapsed_time', 'final_percent', 't60', 'fact_total_esl',
-                                        'status',
-                                       ]
-    draw_imgs_fields_common_report = ['p50', 'p75', 'p90', 'p95', 'p96', 'p97', 'p98', 'p99', 'p995', 'p999', 'p100',
-                                      'not_drawed_esl',
-                                     ]
-    net_compile_fields_common_extended_report = ['create_date_time', 'elapsed_time', 'final_percent', 'status',
-                                                 'p10', 'p20', 'p30', 'p40', 'p50', 'p60', 'p75', 'p90', 'p95', 'p96',
-                                                 'p97', 'p98', 'p99', 'p995', 'p999', 'p100',
-                                                 't10', 't20', 't30', 't40', 't50', 't60', 't70', 't80', 't90', 't100',
-                                                 't110', 't120', 't130', 't140', 't150',
-                                                 'fact_total_esl',
-                                                 'success_percent',
-                                       ]
-    draw_imgs_fields_common_extended_report = ['create_date_time',
-                                               'date_time_finish',
-                                               'draw_imgs_limit_mins',
-                                               'p10', 'p20', 'p30', 'p40', 'p50', 'p60', 'p75', 'p90', 'p95', 'p96',
-                                               'p97', 'p98', 'p99', 'p995', 'p999', 'p100',
-                                               't10', 't20', 't30', 't40', 't50', 't60', 't70', 't80', 't90', 't100',
-                                               't110', 't120', 't130', 't140', 't150',
-                                               'drawed_esl',
-                                               'not_drawed_esl',
-                                               'final_percent',
-                                               'draw_imgs_type',
-                                               'status',
-                                     ]
     response = HttpResponse(
         content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
     )
@@ -96,7 +34,9 @@ def metric_report_export_to_xlsx(request, metric_report_id):
     workbook = create_excel_cheet_for_stats(workbook, net_compile_reports, NetCompilationStat, net_compile_stat_fields)
     workbook = create_excel_cheet(workbook, net_compile_reports, net_compile_reports_draw_fields, vertical=True)
     workbook = create_excel_cheet(workbook, draw_imgs_reports, draw_imgs_reports_draw_fields, vertical=True)
-    workbook = create_excel_cheet(workbook, metrics_report_common_statistic, metrics_report_common_statistic_draw_fields)
+    workbook = create_excel_cheet(workbook,
+                                  metrics_report_common_statistic,
+                                  metrics_report_common_statistic_draw_fields)
     workbook = create_excel_net_draw_cheet(workbook,
                                            net_compile_reports, net_compile_fields_common_extended_report,
                                            draw_imgs_reports, draw_imgs_fields_common_extended_report,
@@ -123,11 +63,6 @@ def chaos_stats_export_to_xlsx(request, chaos_id):
     chaos = Chaos.objects.get(pk=chaos_id)
     net_compile_reports = NetCompileReport.objects.filter(chaos=chaos).order_by('-create_date_time')
     draw_imgs_reports = DrawImgsReport.objects.filter(chaos=chaos).order_by('-create_date_time')
-    net_compile_reports_draw_fields = ['create_date_time', 'status', 'date_time_finish', 'name', 'ip',
-                                       'p50', 'p75', 'p90', 'p95', 'p96', 'p97', 'p98', 'p99', 'p100']
-    draw_imgs_reports_draw_fields = ['create_date_time', 'status', 'date_time_finish', 'name', 'ip',
-                                     'p50', 'p75', 'p90', 'p95', 'p96', 'p97', 'p98', 'p99', 'p100']
-
     response = HttpResponse(
         content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
     )
@@ -136,7 +71,7 @@ def chaos_stats_export_to_xlsx(request, chaos_id):
     )
 
     workbook = Workbook()
-    workbook = create_excel_cheet(workbook, net_compile_reports, net_compile_reports_draw_fields)
-    workbook = create_excel_cheet(workbook, draw_imgs_reports, draw_imgs_reports_draw_fields)
+    workbook = create_excel_cheet(workbook, net_compile_reports, net_compile_short_report_draw_fields)
+    workbook = create_excel_cheet(workbook, draw_imgs_reports, draw_imgs_stats_short_report_draw_fields)
     workbook.save(response)
     return response
